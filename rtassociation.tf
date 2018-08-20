663cecf7ed1adae887d8ad8f5337d69197b100ca
Raghavan R <raghavan.r@capgemini.com>
2018-08-21 09:53:42 +1000

    indent fix

--- rtassociation.tf
+++ rtassociation.tf
@@ -1,6 +1,6 @@
 # Configure Route Table Association
 resource "aws_route_table_association" "rtassociaton" {
-  count          = "${var.create_vpc ? length(var.subnet_id) :0}"
+  count          = "${var.create_vpc ? length(var.subnet_id) : 0}"
   subnet_id      = "${element(var.subnet_id, count.index)}"
   route_table_id = "${element(var.route_table_id, count.index)}"
 }
